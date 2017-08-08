61a049bdad57b32523f7ad23d7515a93722bde73
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-08-08 11:01:18 +0300

    clean up

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 // Returns an array of threats if the arrangement of
 // the pieces is a check, otherwise false
 function isCheck(pieces, player) {
-  return false;
+  return [];
 }
 
 // Returns true if the arrangement of the
 // pieces is a check mate, otherwise false
 function isMate(pieces, player) {
-  return false;
+  return [];
 }
 
 export {
