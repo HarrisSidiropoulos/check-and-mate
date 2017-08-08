661a062aba7a33459434af260c7c88d72b92bc10
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-08-08 18:10:50 +0300

    isMate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable  no-multi-spaces, no-unused-vars, no-console, one-var-declaration-per-line, one-var */
+/* eslint-disable  no-multi-spaces, no-unused-vars, no-console, one-var-declaration-per-line, one-var, default-case */
 const LENGTH = 8;
 
 const KING   = 'king';
@@ -8,38 +8,32 @@ const KNIGHT = 'knight';
 const BISHOP = 'bishop';
 const PAWN   = 'pawn';
 
-function isKing(pos, pieces, player) {
-  return pieces.filter(p => p.piece === KING && p.owner === player && p.x === pos.x && p.y === pos.y).length === 1;
-}
-
-function isEmpty(pos, pieces) {
-  return !pieces.every(p => p.x === pos.x && p.y === pos.y);
-}
-
-function isValidPosition(p) {
-  return p.x >= 0 && p.x < LENGTH && p.y >= 0 && p.y < LENGTH;
-}
+const isEmpty = (pos, pieces) => !pieces.every(p => p.x === pos.x && p.y === pos.y);
+const isKing = (pos, pieces, player) =>
+  pieces.filter(p => p.piece === KING && p.owner === player && p.x === pos.x && p.y === pos.y).length === 1;
+const isValidPosition = (p, piece = { x: -1, y: -1 }) =>
+  p.x >= 0 && p.x < LENGTH && p.y >= 0 && p.y < LENGTH && !(p.x === piece.x && p.y === piece.y);
 
 function checkBishop(piece, pieces, player) {
   let bottomRight = true, topRight = true, bottomLeft = true, topLeft = true, pos;
   for (let i = 0; i < LENGTH; i += 1) {
     pos = { x: i + piece.x, y: i + piece.y };
-    if (bottomRight && isValidPosition(pos)) {
+    if (bottomRight && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       bottomRight = isEmpty(pos, pieces);
     }
     pos = { x: i + piece.x, y: piece.y - i };
-    if (topRight && isValidPosition(pos)) {
+    if (topRight && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       topRight = isEmpty(pos, pieces);
     }
     pos = { x: piece.x - i, y: i + piece.y };
-    if (bottomLeft && isValidPosition(pos)) {
+    if (bottomLeft && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       bottomLeft = isEmpty(pos, pieces);
     }
     pos = { x: piece.x - i, y: piece.y - i };
-    if (topLeft && isValidPosition(pos)) {
+    if (topLeft && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       topLeft = isEmpty(pos, pieces);
     }
@@ -51,22 +45,22 @@ function checkRook(piece, pieces, player) {
   let bottom = true, top = true, left = true, right = true, pos;
   for (let i = 0; i < LENGTH; i += 1) {
     pos = { x: piece.x, y: i + piece.y };
-    if (bottom && isValidPosition(pos)) {
+    if (bottom && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       bottom = isEmpty(pos, pieces);
     }
     pos = { x: piece.x, y: piece.y - i };
-    if (top && isValidPosition(pos)) {
+    if (top && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       top = isEmpty(pos, pieces);
     }
     pos = { x: piece.x - i, y: piece.y };
-    if (left && isValidPosition(pos)) {
+    if (left && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       left = isEmpty(pos, pieces);
     }
     pos = { x: piece.x + i, y: piece.y };
-    if (right && isValidPosition(pos)) {
+    if (right && isValidPosition(pos, piece)) {
       if (isKing(pos, pieces, player)) return true;
       right = isEmpty(pos, pieces);
     }
@@ -123,19 +117,50 @@ function checkPawn(piece, pieces, player) {
 function isCheck(pieces, player) {
   const res = [];
   pieces.filter(p => p.owner !== player).forEach((p) => {
-    if (p.piece === BISHOP && checkBishop(p, pieces, player)) res.push(p);
-    if (p.piece === ROOK   && checkRook(p, pieces, player))   res.push(p);
-    if (p.piece === QUEEN  && checkQueen(p, pieces, player))  res.push(p);
-    if (p.piece === KNIGHT && checkKnight(p, pieces, player)) res.push(p);
-    if (p.piece === PAWN   && checkPawn(p, pieces, player))   res.push(p);
+    switch (p.piece) {
+      case BISHOP:
+        if (checkBishop(p, pieces, player)) res.push(p);
+        break;
+      case ROOK:
+        if (checkRook(p, pieces, player)) res.push(p);
+        break;
+      case QUEEN:
+        if (checkQueen(p, pieces, player)) res.push(p);
+        break;
+      case KNIGHT:
+        if (checkKnight(p, pieces, player)) res.push(p);
+        break;
+      case PAWN:
+        if (checkPawn(p, pieces, player)) res.push(p);
+        break;
+    }
   });
-  return res;
+  return res.length > 0 ? res : false;
 }
 
 // Returns true if the arrangement of the
 // pieces is a check mate, otherwise false
 function isMate(pieces, player) {
-  return [];
+  const king = pieces.filter(p => p.piece === KING && p.owner === player)[0];
+  const playerPieces = pieces.filter(p => p.piece !== KING && p.owner === player);
+  return [
+    { ...king, x: king.x + 1, y: king.y },
+    { ...king, x: king.x - 1, y: king.y },
+    { ...king, x: king.x, y: king.y + 1 },
+    { ...king, x: king.x, y: king.y - 1 },
+    { ...king, x: king.x + 1, y: king.y + 1 },
+    { ...king, x: king.x - 1, y: king.y + 1 },
+    { ...king, x: king.x - 1, y: king.y - 1 },
+    { ...king, x: king.x + 1, y: king.y - 1 },
+  ]
+  .filter(p => isValidPosition(p))
+  .filter(p => playerPieces.every(p1 => !(p1.x === p.x && p1.y === p.y)))
+  .map((k) => {
+    const newPieces = pieces.filter(p => !(p.piece === KING && p.owner === player));
+    newPieces.push(k);
+    return newPieces;
+  })
+  .every(v => isCheck(v, player));
 }
 
 export {
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable one-var, one-var-declaration-per-line, no-console, no-tabs, no-trailing-spaces */
 import { expect } from 'chai';
-import { isCheck } from './index';
+import { isCheck, isMate } from './index';
 
 describe('isCheck', () => {
   describe('Some simple tests', () => {
@@ -144,6 +144,91 @@ describe('isCheck', () => {
         return a.y - b.y;
       };
       expect(isCheck(pieces, 0).sort(sortFunc)).to.be.eql([pieces[5], pieces[6]]); // Double threat
+    });
+  });
+});
+
+describe('isCheck', () => {
+  describe('Some simple tests', () => {
+    it('should return Rook threatens king', () => {
+      /**
+        0 1 2 3 4	5 6 7
+      0					♚
+      1	♜ ♜
+      2
+      3
+      4
+      5
+      6
+      7	♔
+      */
+      const pieces = [
+        { piece: 'king', owner: 1, x: 4, y: 0 },
+        { piece: 'king', owner: 0, x: 0, y: 7 },
+        { piece: 'rook', owner: 1, x: 0, y: 1 },
+        { piece: 'rook', owner: 1, x: 1, y: 1 },
+      ];
+      expect(isMate(pieces, 0)).to.be.eql(true);
+    });
+    it('should return Queen and Pawn threatens king', () => {
+      /**
+        0 1 2 3 4	5 6 7
+      0					♚
+      1
+      2
+      3
+      4
+      5
+      6 ♔    ♟ ♟
+      7	   ♔
+      */
+      const pieces = [
+        { piece: 'king', owner: 1, x: 4, y: 0 },
+        { piece: 'king', owner: 0, x: 2, y: 7 },
+        { piece: 'queen', owner: 1, x: 0, y: 6 },
+        { piece: 'pawn', owner: 1, x: 3, y: 6 },
+        { piece: 'pawn', owner: 1, x: 4, y: 6 },
+      ];
+      expect(isMate(pieces, 0)).to.be.eql(true);
+    });
+    it('should return Queen and Pawn does not threatens king', () => {
+      /**
+        0 1 2 3 4	5 6 7
+      0					♚
+      1
+      2
+      3
+      4
+      5
+      6 ♔    ♟
+      7	   ♔
+      */
+      const pieces = [
+        { piece: 'king', owner: 1, x: 4, y: 0 },
+        { piece: 'king', owner: 0, x: 2, y: 7 },
+        { piece: 'queen', owner: 1, x: 0, y: 6 },
+        { piece: 'pawn', owner: 1, x: 3, y: 6 },
+      ];
+      expect(isMate(pieces, 0)).to.be.eql(false);
+    });
+    it('should return Queen does not threatens king', () => {
+      /**
+        0 1 2 3 4	5 6 7
+      0					♚
+      1
+      2
+      3
+      4
+      5
+      6    ♔
+      7	   ♔
+      */
+      const pieces = [
+        { piece: 'king', owner: 1, x: 4, y: 0 },
+        { piece: 'king', owner: 0, x: 2, y: 7 },
+        { piece: 'queen', owner: 1, x: 2, y: 6 },
+      ];
+      expect(isMate(pieces, 0)).to.be.eql(false);
     });
   });
 });
